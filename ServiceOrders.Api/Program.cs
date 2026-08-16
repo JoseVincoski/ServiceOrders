ef369a78cef15fbb92c280d6c3715ddbdc027342
JoseVincoski <jose.vincoski@gmail.com>
2026-08-16 17:33:17 -0300

    Adicionar arquivos de projeto.

---
+++ ServiceOrders.Api/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using ServiceOrder.Api.Database;
+using ServiceOrder.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(o =>
+    o.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.ApplyMigrations();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
